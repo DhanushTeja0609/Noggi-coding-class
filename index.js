1befe0b010a2720d0c44379a9b7970927610e231
Krithik Ramkumar <krithikr914@gmail.com>
2024-12-10 20:38:50 -0500

    Teaching noggi stuff

---
+++ index.js
@@ -0,0 +1,27 @@
+let x = 9;
+var y = 8;
+const z = 0;
+
+
+/*
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / z);
+
+
+for(let i = 0; i < 10; i++){
+
+    x += 1;
+    console.log(x);
+
+}
+
+*/
+
+while(x < 30){
+
+    x += 1;
+    console.log(x);
+
+}
